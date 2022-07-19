ccaa656190704e9a7ae62e452870913f3f9cd7ae
Faedzatul Faradeanna <2020834582@student.uitm.edu.my>
2022-07-20 07:47:00 +0800

    4

--- microservice.js
+++ microservice.js
@@ -1,23 +1,18 @@
-const pg = require('pg');
+const { Client } = require('pg');
 
-var config = {
-  host: "ec2-34-225-159-178.compute-1.amazonaws.com",
-  user: "wafkmrwskosmvu",
-  password: "216155b0d0503cb0601c13025bf1a5467bb727fc23bb60ff29f5f0c628624b9e",
-  database: "d4ddj7altqepsr"
-}
+const client = new Client({
+  connectionString: "process.env.postgres://wafkmrwskosmvu:216155b0d0503cb0601c13025bf1a5467bb727fc23bb60ff29f5f0c628624b9e@ec2-34-225-159-178.compute-1.amazonaws.com:5432/d4ddj7altqepsr",
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
-const pool = new pg.Pool(config);
+client.connect();
 
-pool.connect()
-    .then(client => {
-        return client.query('SELECT * FROM donation', [1])
-            .then(res => {
-                client.release();
-                console.log(res.rows[0]);
-            })
-            .catch(e => {
-                client.release();
-                console.log(e.stack);
-            })
-  }).finally(() => pool.end());
+client.query('SELECT * FROM donation;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
